c8bee4c355e51544408f4f6554d08868b3cfe51c
Your Name <ashleyp@strategicdata.com.au>
2012-08-03 10:46:58 +1000

    Initial Commit

---
+++ HelloWorldApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HelloWorldApp
+//
+//  Created by Ashley Pope on 3/08/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
